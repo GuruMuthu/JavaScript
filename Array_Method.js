6852cc8473c5b499c9f3f890c3b9e1d9af2b34be
GuruMuthu <muthurvce@gmail.com>
2023-09-05 11:30:26 +0530

    Higher order functions added

--- Array_Method.js
+++ Array_Method.js
@@ -1,69 +1,69 @@
-let a =[1,2,3,4,5]
+// let a =[1,2,3,4,5]
 
-// join is Used to convert the array into string :-
-let al = a.join("|")
-console.log(al)
+// // join is Used to convert the array into string :-
+// let al = a.join("|")
+// console.log(al)
 
-let re = a.map((element)=>{
-    console.log(element)
-    return element
-})
-console.log(re)
+// let re = a.map((element)=>{
+//     console.log(element)
+//     return element
+// })
+// console.log(re)
 
-let ree = a.forEach((element)=>{
-    console.log(element)
-})
-console.log(ree)
+// let ree = a.forEach((element)=>{
+//     console.log(element)
+// })
+// console.log(ree)
 
-let b = a.slice(-3,-1)
-console.log(b)
+// let b = a.slice(-3,-1)
+// console.log(b)
 
-a.splice(2,0,4,5)
-console.log(a)
+// a.splice(2,0,4,5)
+// console.log(a)
 
-console.log(a.toString())
+// console.log(a.toString())
 
-delete(a[5])
-console.log(a)
+// delete(a[5])
+// console.log(a)
 
-let con = a.concat(6,7,8)
-console.log(con)
+// let con = a.concat(6,7,8)
+// console.log(con)
 
-let h = [[1,2,3,4],[6,7,8,9]]
+// let h = [[1,2,3,4],[6,7,8,9]]
 
-let flatt = h.flat()
-console.log(flatt)
-
-
-console.log(flatt.concat("muthu"))
+// let flatt = h.flat()
+// console.log(flatt)
 
 
-let Fahath_Muthu = "hero of maamannan"
-
-try{
-    console.log(udai)
-}
-catch(err){
-    console.log(err)
-}
-finally{
-    console.log(Fahath_Muthu + " is Mr fahadh")
-}
+// console.log(flatt.concat("muthu"))
 
 
-let j = [1,2,3,4]
-for(let i in j){
-switch(j[i]){
-    case 1:
-        console.log("This is ONE")
-        break;
-    case 2:
-        console.log("This is TWO")
-        break;
-    case 3:
-        console.log("This is THREE")
-        break;
-    default:
-        console.log("This is default")
-}}
+// let Fahath_Muthu = "hero of maamannan"
+
+// try{
+//     console.log(udai)
+// }
+// catch(err){
+//     console.log(err)
+// }
+// finally{
+//     console.log(Fahath_Muthu + " is Mr fahadh")
+// }
+
+
+// let j = [1,2,3,4]
+// for(let i in j){
+// switch(j[i]){
+//     case 1:
+//         console.log("This is ONE")
+//         break;
+//     case 2:
+//         console.log("This is TWO")
+//         break;
+//     case 3:
+//         console.log("This is THREE")
+//         break;
+//     default:
+//         console.log("This is default")
+// }}
 
